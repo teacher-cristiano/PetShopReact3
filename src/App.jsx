5777cd5f91c2a46b2f8d24737215edfb319a86f3
Cristiano <professor@SN-371151.edu.fiesc.com.br>
2026-05-12 21:50:34 -0300

    terceiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import NavBar from "./components/NavBar"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import BanhoTosa from "./pages/BanhoTosa"
 
 function App() {
  
   return (
     <>
-     
+    <BrowserRouter>
+     <header>
+      <div className="container">
+        <NavBar />
+      </div>
+     </header>
+
+     <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/banho-e-tosa" element={<BanhoTosa />} />
+      </Routes>
+     </main>
+     </BrowserRouter>
     </>
   )
 }
